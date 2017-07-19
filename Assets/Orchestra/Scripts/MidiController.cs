5e354e1586e6d8c40e83535febc335da0783d8a6
Dizga <alexandre.ganito@gmail.com>
2017-07-19 11:31:31 -0400

    initial Myo supoort

--- Assets/Orchestra/Scripts/MidiController.cs
+++ Assets/Orchestra/Scripts/MidiController.cs
@@ -2,44 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Pose = Thalmic.Myo.Pose;
+
 public class MidiController : MonoBehaviour {
 
+	public GameObject myo = null;
 
 	private bool mouseWasDown = false;
 	private float lastTime = 0.0f;
 
+	// A rotation that compensates for the Myo armband's orientation parallel to the ground, i.e. yaw.
+	// Once set, the direction the Myo armband is facing becomes "forward" within the program.
+	// Set by making the fingers spread pose or pressing "r".
+	private Quaternion _antiYaw = Quaternion.identity;
+
+	// A reference angle representing how the armband is rotated about the wearer's arm, i.e. roll.
+	// Set by making the fingers spread pose or pressing "r".
+	private float _referenceRoll = 0.0f;
+
+	// The pose from the last update. This is used to determine if the pose has changed
+	// so that actions are only performed upon making them rather than every frame during
+	// which they are active.
+	private Pose _lastPose = Pose.Unknown;
+	public float lastAccelValue;
+	private float maxAccelValue;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		lastAccelValue = 0;
+		maxAccelValue = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		if (thalmicMyo.arm == Thalmic.Myo.Arm.Unknown) return; // Ignore input if myo is not being worn
+
 		MIDIPlayer player = GetComponent<MIDIPlayer>();
+		float accelAxis = thalmicMyo.gyroscope.x;
 
-		if (Time.time - lastTime > 30) {
-			lastTime = 0;
-			mouseWasDown = false;
-		}
-		/*FOR NOW: holding t to change tempo, holding v to change volume
-		tempo works by moving the mouse up and down like conductors (Chef d'orchestre ) */
-		if(Input.GetKey("t")){
+		// Has direction changed?
+		bool directionChanged = (accelAxis > 0 && lastAccelValue < 0) || (accelAxis < 0 && lastAccelValue > 0);
+		directionChanged &= Mathf.Abs (accelAxis) > 3; // Deadzone (TODO : Make more reliable)
 
-			if(Input.GetAxis("Mouse Y")<0){
-				mouseWasDown = true;
+		if (directionChanged) {
+			if (lastTime > 0) {
+				player.currentTempo = (uint)(60 / (Time.time - lastTime));
+				print ((uint)(Time.time - lastTime));
 			}
-			if(Input.GetAxis("Mouse Y")>0){
-				if (mouseWasDown) {
-					if (lastTime>0) {
-						player.currentTempo = (uint)(60/(Time.time - lastTime));
-						print ((uint)(Time.time - lastTime));
-					}
-					lastTime = Time.time;
-					mouseWasDown = false;
-				}
-			}
+
+			thalmicMyo.Vibrate (Thalmic.Myo.VibrationType.Short);
+
+			lastTime = Time.time;
+			directionChanged = false;
+			maxAccelValue = 0;
+		} else {
+			maxAccelValue = Mathf.Max(maxAccelValue, Mathf.Abs(accelAxis));
 		}
+
+		lastAccelValue = accelAxis;
+
+		/*
 		if (Input.GetKey ("v")) {
 
 			if (Input.GetAxis ("Mouse X") < 0) {
@@ -53,7 +78,55 @@ public class MidiController : MonoBehaviour {
 					player.velocityScale = 2;
 			}
 		}
+		*/
 
+	}
+
+	// Compute the angle of rotation clockwise about the forward axis relative to the provided zero roll direction.
+	// As the armband is rotated about the forward axis this value will change, regardless of which way the
+	// forward vector of the Myo is pointing. The returned value will be between -180 and 180 degrees.
+	float rollFromZero (Vector3 zeroRoll, Vector3 forward, Vector3 up)
+	{
+		// The cosine of the angle between the up vector and the zero roll vector. Since both are
+		// orthogonal to the forward vector, this tells us how far the Myo has been turned around the
+		// forward axis relative to the zero roll vector, but we need to determine separately whether the
+		// Myo has been rolled clockwise or counterclockwise.
+		float cosine = Vector3.Dot (up, zeroRoll);
+
+		// To determine the sign of the roll, we take the cross product of the up vector and the zero
+		// roll vector. This cross product will either be the same or opposite direction as the forward
+		// vector depending on whether up is clockwise or counter-clockwise from zero roll.
+		// Thus the sign of the dot product of forward and it yields the sign of our roll value.
+		Vector3 cp = Vector3.Cross (up, zeroRoll);
+		float directionCosine = Vector3.Dot (forward, cp);
+		float sign = directionCosine < 0.0f ? 1.0f : -1.0f;
+
+		// Return the angle of roll (in degrees) from the cosine and the sign.
+		return sign * Mathf.Rad2Deg * Mathf.Acos (cosine);
+	}
+
+	// Compute a vector that points perpendicular to the forward direction,
+	// minimizing angular distance from world up (positive Y axis).
+	// This represents the direction of no rotation about its forward axis.
+	Vector3 computeZeroRollVector (Vector3 forward)
+	{
+		Vector3 antigravity = Vector3.up;
+		Vector3 m = Vector3.Cross (myo.transform.forward, antigravity);
+		Vector3 roll = Vector3.Cross (m, myo.transform.forward);
+
+		return roll.normalized;
+	}
+
+	// Adjust the provided angle to be within a -180 to 180.
+	float normalizeAngle (float angle)
+	{
+		if (angle > 180.0f) {
+			return angle - 360.0f;
+		}
+		if (angle < -180.0f) {
+			return angle + 360.0f;
+		}
+		return angle;
 	}
 
 
