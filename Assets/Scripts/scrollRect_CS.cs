787ffbcddb3dc6b574cce59f26f603b6bc538d19
koolen <andre_koolen@hotmail.com>
2017-07-22 09:53:43 -0400

    menu scroll v1

---
+++ Assets/Scripts/scrollRect_CS.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrollRect_CS : MonoBehaviour {
+    public RectTransform panel; //ScroolPanel
+
+    public Button[] btn;
+    public RectTransform center;
+
+    private float[] distance;
+    private bool dragging = false; //true = drag
+    private int btnDistance;
+    private int minButtonNum;
+
+    void Start(){
+        int btnLenght = btn.Length;
+        distance = new float[btnLenght];
+
+        btnDistance = (int)Mathf.Abs(btn[1].GetComponent<RectTransform>() .anchoredPosition.x - btn[0].GetComponent<RectTransform>().anchoredPosition.x);
+  
+    }
+
+    void Update(){
+        for (int i = 0; i < btn.Length; i++) {
+            distance[i] = Mathf.Abs(center.transform.position.x - btn[i].transform.position.x);
+        }
+    }
+}
