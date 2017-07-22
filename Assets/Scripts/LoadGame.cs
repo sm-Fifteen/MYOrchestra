71f1c7df4ddf1209322d2cfef8b95affda2f2d39
koolen <andre_koolen@hotmail.com>
2017-07-22 18:46:01 -0400

    add function loadGame

---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour {
+
+	//public GameObject loadingImage;
+
+    public void LoadScene(string level)
+    {
+        //loadingImage.SetActive(true);
+        SceneManager.LoadScene(level);
+    }
+}
