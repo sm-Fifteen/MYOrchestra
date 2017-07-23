42815ea379e9f4ca23efe5ebce6e28deef2a8a07
Nicolas Roy-Renaud <nicolas.roy-renaud.1@ens.etsmtl.ca>
2017-07-22 23:16:11 -0400

    Removed commented code in midiController

--- Assets/Orchestra/Scripts/MidiController.cs
+++ Assets/Orchestra/Scripts/MidiController.cs
@@ -11,27 +11,12 @@ public class MidiController : MonoBehaviour {
 	public const float LONG_PAUSE_MINIMUM_DURATION = 0.3f;
 
 	public ThalmicMyo thalmicMyo;
+	public Vector2 expectedMovement;
 
-	private bool mouseWasDown = false;
 	private float lastTime = 0.0f;
-
-	// A rotation that compensates for the Myo armband's orientation parallel to the ground, i.e. yaw.
-	// Once set, the direction the Myo armband is facing becomes "forward" within the program.
-	// Set by making the fingers spread pose or pressing "r".
-	private Quaternion _antiYaw = Quaternion.identity;
-
-	// A reference angle representing how the armband is rotated about the wearer's arm, i.e. roll.
-	// Set by making the fingers spread pose or pressing "r".
-	private float _referenceRoll = 0.0f;
-
-	// The pose from the last update. This is used to determine if the pose has changed
-	// so that actions are only performed upon making them rather than every frame during
-	// which they are active.
-	private Pose _lastPose = Pose.Unknown;
 
 	private float maxMagnitude;
 	private bool movingInTheRightDirection = false;
-	public Vector2 expectedMovement;
 
 
 	// Use this for initialization
@@ -96,52 +81,6 @@ public class MidiController : MonoBehaviour {
 			}
 		}
 		*/
-
-		/*******************************************************/
-		/*
-		if (gyroXY.magnitude > MOVEMENT_SPEED_THRESH) {
-            // Regular movement
-            maxMagnitude = Mathf.Max(maxMagnitude, gyroXY.magnitude);
-            alreadyPausing = false;
-		} else if (!alreadyPausing) {
-			// Movement has stopped (and wasn't stopping before)
-			Debug.Log("Paused");
-			alreadyPausing = true;
-			updateTempo = true;
-		}
-
-		if (updateTempo && (Time.time - lastTime) > MINIMUM_MOVEMENT_TIME) {
-			if (lastTime > 0) {
-				player.currentTempo = (uint)(60 / (Time.time - lastTime));
-				Debug.LogWarning (Time.time - lastTime);
-			}
-
-			thalmicMyo.Vibrate (Thalmic.Myo.VibrationType.Short);
-
-			lastTime = Time.time;
-			maxMagnitude = 0;
-		} else if (alreadyPausing && (Time.time - lastTime) > LONG_PAUSE_MINIMUM_DURATION) {
-			// TODO : Pause immediately if hand is raised
-			player.currentTempo = (uint)(60 / (Time.time - lastTime));
-		}
-		*/
-
-		/*
-		if (Input.GetKey ("v")) {
-
-			if (Input.GetAxis ("Mouse X") < 0) {
-				player.velocityScale = player.velocityScale - 0.02f;
-				if (player.velocityScale < 0)
-					player.velocityScale = 0;
-			}
-			if (Input.GetAxis ("Mouse X") > 0) {
-				player.velocityScale = player.velocityScale + 0.02f;
-				if (player.velocityScale > 2)
-					player.velocityScale = 2;
-			}
-		}
-		*/
-
 	}
 
 	private Vector2 nextMovement(Thalmic.Myo.Arm conductingArm) {
