6e84d5deeec891d19d60f83ec4272cc3dd09cc20
Riboshom <riboshom@hotmail.com>
2017-07-25 17:04:10 -0400

    Added a menu button prefab

---
+++ Assets/OrchestraMenu/Scripts/MenuButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class MenuButton : MonoBehaviour {
+	private Text songNameUI;
+	private Text songComposerUI;
+
+	public string midiPath = "";
+	public string songName = "";
+	public string songComposer = "";
+
+	void Awake () {
+		foreach (Text textElement in GetComponentsInChildren<Text>()) {
+			if (textElement.CompareTag ("SongName")) {
+				songNameUI = textElement;
+			} else if (textElement.CompareTag ("ArtistName")) {
+				songComposerUI = textElement;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void OnValidate () {
+		songNameUI.text = songName;
+		songComposerUI.text = songComposer;
+	}
+}
